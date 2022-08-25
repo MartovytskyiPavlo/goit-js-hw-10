e3acb9fd3dcb1214b36693e107aa9c83fd45af7a
MartovytskyiPavlo <viy.1979@gmail.com>
2022-08-25 23:08:25 +0300

    start

--- src/index.js
+++ src/index.js
@@ -30,11 +30,54 @@ function renderCard(data) {
     } else if (data.length > 10) {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     } else if (data.length > 1) {
-        countryList.innerHTML = "<pre>" + JSON.stringify(data, null, "  ") + "</pre>";
+        countryList.innerHTML = prepareList(data);
+        countryInfo.innerHTML = "";
+    } else {
+        console.log(data);
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = prepareInfo(data[0]);
     }
 }
 
 function fetchCountries(name) {
     return fetch(API_URL + name + "?fields=" + filterFields.join(",")).then(r => r.json())
+}
+
+
+function getLanguages(languages) { 
+    let result = "";
+
+    for (let key in languages) {
+        result = result + languages[key] + ", ";
+    }
+
+    return result.slice(0, -2);    
+}
+
+
+function getOneCountry(country) {
+    return `<div class="Container">
+                <img alt="${country.name.official}" src="${country.flags.png}" class='Flag'></img>
+                <div><b>${country.name.official}</b></div>
+            </div>`;
+}
+
+function prepareList(data) {
+    let result = "";
+    for (const item of data) {
+        result += getOneCountry(item);
+    }
+
+    return result;
+}
+
+function prepareInfo(data) {
+    return `<div class="Container">
+                <img alt="${data.name.official}" src="${data.flags.png}" class='Flag'></img>
+                <div class="Country"><b>${data.name.official}</b></div>
+            </div>
+            <div class='Capital'><b>Capital: </b>${data.capital}</div>
+            <div class='Population'><b>Country: </b>${data.population}</div>
+            <div class='Languages'><b>Languages: </b>${getLanguages(data.languages)}</div>`;
 }
 
